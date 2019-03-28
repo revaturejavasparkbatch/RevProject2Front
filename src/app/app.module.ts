ca604b14f1ad2a3ae4babab91d9dfc24eba3200a
vijaysadanani <vksadanani@gmail.com>
2019-03-27 23:19:01 -0400

    Added modals, installed necessary packages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,32 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { LoginViewComponent } from './components/login-view/login-view.component';
+import { HomeComponent } from './components/home/home.component';
+
+import {MatDialogModule} from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CreateAccountComponent } from './components/create-account/create-account.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ModalviewComponent } from './components/modalview/modalview.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginViewComponent
+    LoginViewComponent,
+    HomeComponent,
+    CreateAccountComponent,
+    ModalviewComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule 
+    AppRoutingModule,
+    MatDialogModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [LoginViewComponent]
 })
 export class AppModule { }
