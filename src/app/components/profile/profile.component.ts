e3ed9c3b264e8ee79a52f9b46412edff6b23af03
vijaysadanani <vksadanani@gmail.com>
2019-03-30 19:36:07 -0400

    Added profile routing and fortune cards

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: Router) { }
 
   ngOnInit() {
   }
 
+  fortunes: String[] = ["hi", "bye"];
+
+
+  goDash() {
+    this.route.navigateByUrl("/dashboard");
+  }
+
+  logout(){
+    this.route.navigateByUrl("/");
+  }
 }
