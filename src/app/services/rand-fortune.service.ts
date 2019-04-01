de7fc971f757e1744bfebc4ed9e23ac6230693c4
Peyton Shriver <31674473+peytonshriver@users.noreply.github.com>
2019-04-01 11:30:27 -0400

    added user information to generate random fortune, such that random fortune will exclude favortied fortunes

--- src/app/services/rand-fortune.service.ts
+++ src/app/services/rand-fortune.service.ts
@@ -2,23 +2,27 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Fortune } from '../Fortune';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
+import { UserCarryService } from 'src/app/services/user-carry.service';
+import { loggedInUser } from 'src/app/loggedInUser';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RandFortuneService {
 
-
-  getRandFortune(): Observable<Fortune[]> {
-    let fortuneNum = this.getNumRand();
+  getRandFortune(fortuneNums): Observable<Fortune[]> {
+    let fortuneNum = this.getNumRand(fortuneNums);
     let randomFortuneUrl = "http://fortunecookieapi.herokuapp.com/v1/fortunes?limit=1&skip=" + fortuneNum;
     return this.http.get<Fortune[]>(randomFortuneUrl);
   }
 
-  getNumRand(): Number{
+  getNumRand(fortuneNums): Number{
     let numarr = [];
+    console.log(fortuneNums);
     for(let i=0; i<547; ++i){
+      if(!fortuneNums.includes(i)){
       numarr.push(i);
+      }
     }
     let num = numarr[Math.floor(Math.random()*numarr.length)];
     return num;
